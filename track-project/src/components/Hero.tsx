87936c8025d0383f0d521ffb7443e67df43be704
Hamse Arwo <arwohamse@gmail.com>
2025-09-21 13:42:29 -0400

    Completed home page

--- track-project/src/components/Hero.tsx
+++ track-project/src/components/Hero.tsx
@@ -1,13 +1,44 @@
 import HeroImage from "../assets/hero.svg";
+import trackLogo from "../assets/log.png";
+import futureLogo from "../assets/long-term.png";
+import manageLogo from "../assets/bar-chart.png";
+import informLogo from "../assets/alarm.png";
 import "../App.css";
 
 function Hero() {
+  const heroItems = [
+    {
+      title: "Invest For Your Future",
+      img: futureLogo,
+    },
+    {
+      title: "Simple Growth Insights",
+      img: manageLogo,
+    },
+    {
+      title: "Manage Your Portfolio",
+      img: trackLogo,
+    },
+    {
+      title: "Contribution Reminders",
+      img: informLogo,
+    },
+  ];
+
   return (
     <div className="hero">
       <div className="hero-text">
         <h2>WELCOME TO</h2>
         <h1>Trackvestment</h1>
         <h2>Track your investments and stay on top of your finances.</h2>
+        <div className="hero-items">
+          {heroItems.map((item) => (
+            <div className="hero-item" key={item.title}>
+              <img src={item.img} alt={item.title} />
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
         <div className="hero-btn">
           <button>Sign Up Now</button>
 
